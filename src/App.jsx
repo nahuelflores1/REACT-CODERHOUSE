4f638e935bf1b3a2434193b5d6cd92072bdfbd75
nahuelflores1 <nahuelflores2018@hotmail.com>
2023-06-08 20:49:24 -0300

    Primer archivo REACT

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./Navbar";
+import ProductList from "./ProductsList";
+import Footer from "./footer";
+
+function App() {
+    return (
+        <div>
+            <Navbar />
+            <ProductList />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = ()=>{
+
+    return <header>
+        <ul>
+            <il>Inicio</il>
+            <il>Contacto</il>
+            <il>Sobre Mi</il>
+        </ul>
+    </header>
+    
+}
+
+export default Navbar
